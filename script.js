8ec4e9223df5c856ece4473c4edfd3e4e7716560
hiromi-in <piromi.ishikawa@gmail.com>
2022-06-09 16:00:35 -0500

    Change textCONTENT to innerHTML.

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ for (let i=2; i<=n; i++){
     for(let j=2; j<=i; j++){
         if(i%j===0) continue
     }
-    document.getElementById('prime_number').textContent=<li> +i +</li>;
+    document.getElementById('prime_number').innerHTML=`<li>${i}</li>`;
 }
+
