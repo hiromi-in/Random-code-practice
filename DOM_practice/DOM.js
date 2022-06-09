552785f9e88f3cbbf650afd5f1c2e0bc51e69806
hiromi-in <piromi.ishikawa@gmail.com>
2022-06-09 16:46:47 -0500

    Edit the first exercise.

--- DOM_practice/DOM.js
+++ DOM_practice/DOM.js
@@ -6,3 +6,20 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const p = document.createElement('p');
+p.classList.add('para')
+container.appendChild(p);
+p.style.color='red';
+p.innerHTML = "<span>Hey I'm red!</span>";  
+
+
+/*
+a <p> with red text that says “Hey I’m red!”
+an <h3> with blue text that says “I’m a blue h3!”
+a <div> with a black border and pink background color with the following elements inside of it:
+another <h1> that says “I’m in a div”
+a <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+*/
+
