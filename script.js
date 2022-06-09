551b733a71262ae1c511e1fdcc7638bd57612c6e
hiromi-in <piromi.ishikawa@gmail.com>
2022-06-09 14:13:13 -0500

    Create html,js, css files.

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Look for prime numbers
+let n=parseInt(window.prompt('Please type a number'));
+
+for (let i=2; i<=n; i++){
+    for(let j=2; j<=i; j++){
+        if(i%j===0) continue
+    }
+    document.getElementById('prime_number').textContent=<li> +i +</li>;
+}
